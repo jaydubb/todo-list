da5451018df64a0d5efec96188daa324b4669baa
Jay Wyche <jwyche@gmail.com>
2025-12-13 15:55:53 -0500

    Add todo functionality added to form component

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,8 +1,20 @@
-const TodoForm = () =>{
+import { useRef } from "react";
+
+const TodoForm = ({onAddTodo}) =>{
+    const todoTitleInput = useRef("");
+
+    const handleAddTodo = (event) => {
+        event.preventDefault();
+        const title = event.target.title.value;
+        onAddTodo(title);
+        event.target.title.value = "";
+        todoTitleInput.current.focus();
+    };
+
     return (
-        <form name="add_todo">
+        <form name="add_todo" onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Todo</label>
-            <input type="text" name="todoTitle"></input>
+            <input type="text" name="title" ref={todoTitleInput}></input>
             <button type="submit">Add Todo</button>
         </form>
     );
